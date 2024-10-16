4cbc83f419c8b9f677d89a239c5a64ef70357d49
Emma001code <e.ngwoke@alustudent.com>
2024-10-16 10:54:33 -0700

    Emma backup script

--- backup_archives.sh
+++ backup_archives.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+userinfo="2d294d8d9a24"
+hostinfo="2d294d8d9a24.601a9382.alu-cod.online"
+
 archDir="archived_logs_group19"
 
-if [ ! -d "archDir" ]; then
+if [ ! -d "myarchDir" ]; then
+        mkdir "$myarchDir"
+else
+        echo "Emma the directory already exist!"
+fi
+
+for txtfile in heart_rate_log.txt_*; do
+        if [ ! -f "$txtfile" ]; then
+                echo "Emma you are awesome all is backed up!"
+        else
+                mv "$txtfile" "myarchDir"
+        fi
+done
+
+scp -r $myarchDir $userinfo@$host:/home/
+
+if [ $? -eq 0 ]; then
+        echo "yoh Emma you did it"
+else
+        echo "sorry bro go fix your script"
+fi

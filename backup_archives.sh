f949af35789c120a02dc458d51cf82fab5c38363
Emma001code <e.ngwoke@alustudent.com>
2024-10-20 05:58:55 -0700

    checking the functionality

--- backup_archives.sh
+++ backup_archives.sh
@@ -5,24 +5,34 @@ hostinfo="5fd809c6916d.d4b45129.alu-cod.online"
 
 myarchDir="archived_logs_group19"
 
+# Create the directory if it doesn't exist
 if [ ! -d "$myarchDir" ]; then
         mkdir "$myarchDir"
 else
-        echo "Emma the directory already exist!"
+        echo "Emma, the directory already exists!"
 fi
 
-for txtfile in heart_rate_log.txt_*; do
-        if [ ! -f "$txtfile" ]; then
-                echo "Emma you are awesome all is backed up!"
-        else
-                mv "$txtfile" "$myarchDir"
+# Check if any files matching the pattern exist
+echo "Looking for files matching pattern heart_rate_log.txt_*"
+if ls heart_rate_log.txt_* 1> /dev/null 2>&1; then
+    for txtfile in heart_rate_log.txt_*; do
+        if [ -f "$txtfile" ]; then
+            mv "$txtfile" "$myarchDir"
         fi
-done
-
-scp -r $myarchDir $userinfo@$hostinfo:/home/
-
-if [ $? -eq 0 ]; then
-        echo "yoh Emma you did it"
+    done
+    echo "Files moved to $myarchDir."
 else
-        echo "sorry bro go fix your script"
+    echo "Emma, you are awesome! All is already backed up!"
 fi
+
+# Secure copy to remote server
+scp -r "$myarchDir" "$userinfo@$hostinfo:/home/"
+
+# Check if scp was successful
+if [ $? -eq 0 ]; then
+    echo "Yoh Emma, you did it!"
+else
+    echo "Sorry bro, go fix your script!"
+fi
+
+
